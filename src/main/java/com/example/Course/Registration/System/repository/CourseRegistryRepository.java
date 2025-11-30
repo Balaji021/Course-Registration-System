70d5d7eb52ed7ccb9e30c5aed006d73550121d94
Balaji021 <balajiselvaraj12@gmail.com>
2025-11-30 16:13:35 +0530

    removed

--- src/main/java/com/example/Course/Registration/System/repository/CourseRegistryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Course.Registration.System.repository;
-
-import com.example.Course.Registration.System.model.Course_Registry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRegistryRepository extends JpaRepository<Course_Registry,Integer> {
-}
